c53db1e5df275ab74fc482d73b5c974a4c24615c
hwacho9 <csh77776@gmail.com>
2024-04-02 10:25:48 +0900

    feat:refactor

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const StartScreen(),
+      home: Scaffold(
+        body: StartScreen(),
+      ),
     );
   }
 }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,37 +5,32 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text('Flutter Demo'),
-      // ),
-      body: Container(
-        color: Colors.purple,
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(
-                ('assets/images/quiz-logo.png'),
-                width: 300,
+    return Container(
+      color: Colors.purple,
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              ('assets/images/quiz-logo.png'),
+              width: 300,
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            const Text("Leart Flutter the fun way!",
+                style: TextStyle(fontSize: 30, color: Colors.white)),
+            const SizedBox(
+              height: 20,
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child: const Text(
+                "Start Quiz",
+                style: TextStyle(fontSize: 20, color: Colors.white),
               ),
-              const SizedBox(
-                height: 60,
-              ),
-              const Text("Leart Flutter the fun way!",
-                  style: TextStyle(fontSize: 30, color: Colors.white)),
-              const SizedBox(
-                height: 20,
-              ),
-              OutlinedButton(
-                onPressed: () {},
-                child: const Text(
-                  "Start Quiz",
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
