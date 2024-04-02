7c031baba132717085c96d6962381a47e11d1209
hwacho9 <csh77776@gmail.com>
2024-04-02 10:17:47 +0900

    feat:button

--- lib/main.dart
+++ lib/main.dart
@@ -34,10 +34,23 @@ class MyHomePage extends StatelessWidget {
           child: Column(mainAxisSize: MainAxisSize.min, children: [
             Image.asset(
               ('assets/images/quiz-logo.png'),
-              width: 200,
+              width: 300,
+            ),
+            const SizedBox(
+              height: 60,
             ),
             const Text("Leart Flutter the fun way!",
-                style: TextStyle(fontSize: 20, color: Colors.white)),
+                style: TextStyle(fontSize: 30, color: Colors.white)),
+            const SizedBox(
+              height: 20,
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child: const Text(
+                "Start Quiz",
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ),
           ]),
         ),
       ),
