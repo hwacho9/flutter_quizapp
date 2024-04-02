d08e66aed5639eef54e0d5c17c790daa0e6de90f
hwacho9 <csh77776@gmail.com>
2024-04-02 10:22:30 +0900

    refactor:start screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/start_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,46 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text('Flutter Demo'),
-      // ),
-      body: Container(
-        color: Colors.purple,
-        child: Center(
-          child: Column(mainAxisSize: MainAxisSize.min, children: [
-            Image.asset(
-              ('assets/images/quiz-logo.png'),
-              width: 300,
-            ),
-            const SizedBox(
-              height: 60,
-            ),
-            const Text("Leart Flutter the fun way!",
-                style: TextStyle(fontSize: 30, color: Colors.white)),
-            const SizedBox(
-              height: 20,
-            ),
-            OutlinedButton(
-              onPressed: () {},
-              child: const Text(
-                "Start Quiz",
-                style: TextStyle(fontSize: 20, color: Colors.white),
-              ),
-            ),
-          ]),
-        ),
-      ),
+      home: const StartScreen(),
     );
   }
 }
---
+++ lib/start_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text('Flutter Demo'),
+      // ),
+      body: Container(
+        color: Colors.purple,
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                ('assets/images/quiz-logo.png'),
+                width: 300,
+              ),
+              const SizedBox(
+                height: 60,
+              ),
+              const Text("Leart Flutter the fun way!",
+                  style: TextStyle(fontSize: 30, color: Colors.white)),
+              const SizedBox(
+                height: 20,
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                child: const Text(
+                  "Start Quiz",
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
